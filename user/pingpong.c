29204be01ad48b9eabcdb4f1fd6980d7e80c6901
yang <xingguoyang_mails@163.com>
2023-05-21 10:36:37 +0800

    finish pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#define ONEBETY 1
+int main(){
+    int fd_fw[2];
+    int fd_cw[2];
+    char buf[ONEBETY];
+    pipe(fd_fw);
+    pipe(fd_cw);
+
+    int pid=fork();
+    if(pid>0){
+        write(fd_fw[0],"ping",ONEBETY);
+        read(fd_cw[1],buf,ONEBETY);
+        printf("<%d>:received %s",getpid(),buf);
+    }
+    else
+    {
+        read(fd_fw[1],buf,ONEBETY);
+        printf("<%d>:received %s",getpid(),buf);
+        write(fd_cw[0],"pong",ONEBETY);
+        exit();
+    }
+    return 0;
+}
