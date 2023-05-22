a7c89965a593fec3f21f41ff24fb702de82f30e3
yang <xingguoyang_mails@163.com>
2023-05-22 15:35:26 +0800

    finish find

--- user/find.c
+++ user/find.c
@@ -15,76 +15,80 @@
  */
 
 
-char* fmtname(char * path){
+char*
+fmtname(char *path)
+{
     static char buf[DIRSIZ+1];
     char *p;
 
-    //Find first character after last slash
-    //查找最后一个斜线后的第一个字符
-    for(p=path+strlen(path);p>=path&&*p!='/';p--);
+    // Find first character after last slash.
+    for(p=path+strlen(path); p >= path && *p != '/'; p--)
+        ;
     p++;
 
-    //Return blank-padded name.
-    //返回空白填充的名称。
-    if(strlen(p)>=DIRSIZ)
+    // Return blank-padded name.
+    if(strlen(p) >= DIRSIZ)
         return p;
-    memmove(buf,p, strlen(p));
-    buf[stelen(p)]=0;
+    memmove(buf, p, strlen(p));
+    buf[strlen(p)] = 0;
     return buf;
 }
 
-void find(char *path,char *filename){
-    char buf[512],*p;
+void find(char *path,char *fileName){
+    //printf("path:%s fileName:%s\n", fmtname(path),fileName);
+    char buf[512], *p;
     int fd;
     struct dirent de;
     struct stat st;
 
-    if((fd=open(path,0))<0){
+    if((fd = open(path, 0)) < 0){
         fprintf(2, "find: cannot open %s\n", path);
         return;
     }
 
-    if(fstat(fd,&st)<0){
-        ffprintf(2, "find: cannot stat %s\n", path);
+    if(fstat(fd, &st) < 0){
+        fprintf(2, "find: cannot stat %s\n", path);
         close(fd);
         return;
     }
-    switch (st.type) {
-        case T_FILE:// if path is a file,then compare their fileName
-            if(strcmp(fmtname(path),filename)==0)
+
+    switch(st.type){
+        case T_FILE: // if path is a file,then compare their fileName
+            if(strcmp(fmtname(path),fileName) == 0)
                 printf("%s\n",path);
             break;
+
         case T_DIR:
-            if(strlen(path)+1+DIRSIZ+1>sizeof(buf)){
-                printf("find:path too long\n");
+            if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+                printf("find: path too long\n");
                 break;
             }
-            strcpy(buf,path);
-            p=buf+strlen(buf);
-            *p++='/';// now buf contains a subdirectory
-            while(read(fd,&de,sizeof(de))==sizeof(de)) {//continue to read file from path
-                if (de.inum == 0 || strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+            strcpy(buf, path);
+            p = buf+strlen(buf);
+            *p++ = '/'; // now buf contains a subdirectory
+            while(read(fd, &de, sizeof(de)) == sizeof(de)){ //continue to read file from path
+                if(de.inum == 0 || strcmp(de.name, ".")==0 || strcmp(de.name, "..")==0)
                     continue;
-                memmove(p,de.name,DIRSIZ);
-                p[DIRSIZ]=0;
-                if(fstat(buf,&st)<0){
+                memmove(p, de.name, DIRSIZ);
+                p[DIRSIZ] = 0;
+                if(stat(buf, &st) < 0){
                     printf("find: cannot stat %s\n", buf);
                     continue;
                 }
-                find(buf,filename);
+                find(buf,fileName);
             }
             break;
     }
     close(fd);
 }
-
-int main(int argc,int *argv[])
+int
+main(int argc, char *argv[])
 {
-    if(argc!=3){
+    if(argc != 3) {
         printf("usage: find <path> <file name>\n");
         exit(1);
     }
-    find(argc[1],argv[2]);
+    find(argv[1],argv[2]);
     exit(0);
     return 0;
 }
