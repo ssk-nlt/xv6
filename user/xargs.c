1f6dc402934dc0ab894ec3c8e59b46cd9dfe6228
yang <xingguoyang_mails@163.com>
2023-05-22 20:15:32 +0800

    finish xargs

--- user/xargs.c
+++ user/xargs.c
@@ -16,41 +16,86 @@ xargs完成了两个行为：
 
 #define MSGSIZE 16
 
-//$echo hello too | xargs echo bye
-int main(int argc,char *argv[]){
-    sleep(30);   //有时候点一个命令没有执行完，所以等待其输出到标准化输出
+////$echo hello too | xargs echo bye
+//int main(int argc,char *argv[]){
+//    sleep(30);   //有时候点一个命令没有执行完，所以等待其输出到标准化输出
+//
+//    //Q1如何获取前一个命令的标准化输出（即此命令的标准化输入）呢？
+//    char buf[MSGSIZE];
+//    read(0,buf,MSGSIZE);
+//
+//    //Q2如何获取到自己的命令行参数
+//    char *xargv[MAXARG];
+//    int xargc=0;
+//    for(int i=1;i<argc;++i){  //xargs echo bye
+//        xargv[xargc]=argv[i];
+//        xargc++;
+//    }
+//    char *p=buf;
+//    for(int i=0;i<MSGSIZE;++i){
+//        if(buf[i]=='\n'){
+//            int pid=fork();
+//            if(pid>0){
+//                p=&buf[i+1];
+//                wait(0);
+//            }else{
+//                //Q3如何使用exec取执行命令
+//                buf[i]=0;// set '\0' to end of token
+//                xargv[xargc]=p;
+//                xargc++;
+//                xargv[xargc]=0;// set nullptr in the end of array
+//                xargc++;
+//
+//                exec(xargv[0],xargv);  //echo bye hello too
+//                exit(0);
+//            }
+//        }
+//    }
+//    wait(0);
+//    exit(0);
+//}
+int
+main(int argc, char *argv[])
+{
+    char inputBuf[32]; // record the input from previous command
+    char charBuf[320]; // buf for the char of all token
+    char* charBufPointer = charBuf;
+    int charBufSize = 0;
 
-    //Q1如何获取前一个命令的标准化输出（即此命令的标准化输入）呢？
-    char buf[MSGSIZE];
-    read(0,buf,MSGSIZE);
+    char *commandToken[32]; // record the token from input spilt by space(' ')
+    int tokenSize = argc - 1;// record token number(initial is argc - 1,because xargs will bot be execute)
+    int inputSize = -1;
 
-    //Q2如何获取到自己的命令行参数
-    char *xargv[MAXARG];
-    int xargc=0;
-    for(int i=1;i<argc;++i){  //xargs echo bye
-        xargv[xargc]=argv[i];
-        xargc++;
-    }
-    char *p=buf;
-    for(int i=0;i<MSGSIZE;++i){
-        if(buf[i]=='\n'){
-            int pid=fork();
-            if(pid>0){
-                p=&buf[i+1];
+    // first copy initial argv argument to commandToken
+    for(int tokenIdx=0;tokenIdx<tokenSize;tokenIdx++)
+        commandToken[tokenIdx]=argv[tokenIdx+1];
+
+
+    while((inputSize = read(0,inputBuf,sizeof(inputBuf))) > 0){
+        for(int i = 0; i < inputSize;i++){
+            char curChar = inputBuf[i];
+            if(curChar == '\n'){// if read '\n', execute the command
+                charBuf[charBufSize] = 0; // set '\0' to end of token
+                commandToken[tokenSize++] = charBufPointer;
+                commandToken[tokenSize] = 0; // set nullptr in the end of array
+
+                if(fork()==0){ // open child to execute
+                    exec(argv[1],commandToken);
+                }
                 wait(0);
-            }else{
-                //Q3如何使用exec取执行命令
-                buf[i]=0;// set '\0' to end of token
-                xargv[xargc]=p;
-                xargc++;
-                xargv[xargc]=0;// set nullptr in the end of array
-                xargc++;
-
-                exec(xargv[0],xargv);  //echo bye hello too
-                exit(0);
+                tokenSize = argc - 1;// initialize
+                charBufSize = 0;
+                charBufPointer = charBuf;
             }
+            else if(curChar == ' '){
+                charBuf[charBufSize++] = 0; // mark the end of string
+                commandToken[tokenSize++] = charBufPointer;
+                charBufPointer = charBuf+charBufSize; // change to the start of new string
+            }else{
+                charBuf[charBufSize++] = curChar;
+            }
+
         }
     }
-    wait(0);
     exit(0);
 }
