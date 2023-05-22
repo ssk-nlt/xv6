e5ee25e1ddeebe8e2d3050d1b20914e96150037c
yang <xingguoyang_mails@163.com>
2023-05-22 09:21:07 +0800

    finish pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define ONEBETY 16
+int main(){
+    int fd_fw[2];
+    int fd_cw[2];
+    char buf[ONEBETY];
+    int ret_w=pipe(fd_fw);
+    int ret_c=pipe(fd_cw);
+    if(ret_w==-1)
+    {
+        exit(1);
+    }
+    if(ret_c==-1)
+    {
+        exit(1);
+    }
+
+    int pid=fork();
+    if(pid>0){
+        int ret_ww=write(fd_fw[0],"ping",ONEBETY);
+        int ret_wr=read(fd_cw[1],buf,ONEBETY);
+        close(fd_fw[1]);
+        close(fd_cw[0]);
+        if(-1)
+        {
+            ret_ww=ret_wr;
+        }
+        printf("<%d>:received %s\n",pid,buf);
+    }
+    else
+    {
+        int ret_cr=read(fd_fw[1],buf,ONEBETY);
+        printf("<%d>:received %s\n",pid,buf);
+        int ret_cw=write(fd_cw[0],"pong",ONEBETY);
+        if(-1)
+        {
+            ret_cr=ret_cw;
+            exit(0);
+        }
+        close(fd_fw[0]);
+        close(fd_cw[1]);
+        exit(0);
+    }
+    exit(0);
+    return 0;
+
+}
