748d5b5a3fc841d57f95b6bd4a968f30f15d8108
yang <xingguoyang_mails@163.com>
2023-05-30 10:21:01 +0800

    finish speed up system calls

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,14 @@ found:
     return 0;
   }
 
+  //todo Allocate an usyscall page
+    if((p->usyscall = (struct usyscall *)kalloc()) == 0){
+        freeproc(p);
+        release(&p->lock);
+        return 0;
+    }
+    p->usyscall->pid=p->pid;
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -153,6 +161,9 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+  if(p->usyscall)
+      kfree((void*)p->trapframe);
+  p->usyscall=0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -164,7 +175,6 @@ freeproc(struct proc *p)
   p->killed = 0;
   p->xstate = 0;
   p->state = UNUSED;
-  p->trace_mask=0;
 }
 
 // Create a user page table for a given process,
@@ -197,6 +207,15 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // todo map the usyscalll below TRAMPOLINE
+    if(mappages(pagetable, USYSCALL, PGSIZE,
+                (uint64)(p->usyscall), PTE_R | PTE_U) < 0){
+        uvmunmap(pagetable,USYSCALL,1,0);
+        uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+        uvmfree(pagetable, 0);
+        return 0;
+    }
+
   return pagetable;
 }
 
@@ -205,9 +224,11 @@ proc_pagetable(struct proc *p)
 void
 proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
-  uvmunmap(pagetable, TRAMPOLINE, 1, 0);
-  uvmunmap(pagetable, TRAPFRAME, 1, 0);
-  uvmfree(pagetable, sz);
+    // todo unmap usyscall page
+    uvmunmap(pagetable,USYSCALL,1,0);
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, sz);
 }
 
 // a user program that calls exec("/init")
@@ -229,7 +250,7 @@ userinit(void)
   struct proc *p;
 
   p = allocproc();
-  initproc = p;
+  initproc = p;z
   
   // allocate one user page and copy init's instructions
   // and data into it.
@@ -295,9 +316,6 @@ fork(void)
 
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
-
-  //copy trace_mask in the child
-  np->trace_mask=p->trace_mask;
 
   // increment reference counts on open file descriptors.
   for(i = 0; i < NOFILE; i++)
