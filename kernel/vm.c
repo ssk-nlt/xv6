b0555cd091b1410f94879bb256a0f7e6047e563e
yang <xingguoyang_mails@163.com>
2023-05-31 10:33:33 +0800

    finish Print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -433,6 +433,28 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
   }
 }
 
+static char *prefix[]={
+        [0]="..",
+        [1]=".. ..",
+        [2]=".. .. .."
+};
+
 void vmprint(pagetable_t pagetable,uint64 depth){
-    printf("vmprint say hi\n");
+    if(depth>2)
+    {
+        return;
+    }
+    if(depth==0)
+    {
+        printf("page table %p\n",pagetable);
+    }
+    char *buf=prefix[depth];
+    for(int i = 0; i < 512; i++){
+        pte_t pte = pagetable[i];
+        if(pte & PTE_V){
+            printf("%s%d: pte %p pa %p\n",buf,i,pte, PTE2PA(pte));
+            uint64 child = PTE2PA(pte);
+            vmprint((pagetable_t) child,depth+1);
+        }
+    }
 }
