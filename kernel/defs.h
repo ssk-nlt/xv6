8f328da05ddfb16d31338f7e81a2192182bb33de
yang <xingguoyang_mails@163.com>
2023-05-31 10:33:33 +0800

    finish Print a page table

--- kernel/defs.h
+++ kernel/defs.h
@@ -105,6 +105,7 @@ int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
 
+
 // swtch.S
 void            swtch(struct context*, struct context*);
 
@@ -170,6 +171,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+int             vm_pgaccess(pagetable_t pagetable,uint64 va);
 
 // plic.c
 void            plicinit(void);
--- kernel/vm.c
+++ kernel/vm.c
@@ -433,6 +433,43 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
   }
 }
 
+static char *prefix[]={
+        [0]="..",
+        [1]=".. ..",
+        [2]=".. .. .."
+};
+
 void vmprint(pagetable_t pagetable,uint64 depth){
-    printf("vmprint say hi\n");
+    if(depth>2)
+    {
+        return;
+    }
+    if(depth==0)
+    {
+        printf("page table %p\n",pagetable);
+    }
+    char *buf=prefix[depth];
+    for(int i = 0; i < 512; i++){
+        pte_t pte = pagetable[i];
+        if(pte & PTE_V){
+            printf("%s%d: pte %p pa %p\n",buf,i,pte, PTE2PA(pte));
+            uint64 child = PTE2PA(pte);
+            vmprint((pagetable_t) child,depth+1);
+        }
+    }
+}
+
+int vm_pgaccess(pagetable_t pagetable,uint64 va){
+    pte_t *pte;
+    if(va>=MAXVA)
+        return 0;
+
+    pte=walk(pagetable,va,0);
+    if(pte==0)
+        return 0;
+    if((*pte&PTE_A)!=0){
+        *pte=*pte&(~PTE_A);   //clear 6th flag(PTE_A)
+        return 1;
+    }
+    return 0;
 }
