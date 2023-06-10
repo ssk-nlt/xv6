7bf49126d067b7d1feda020c976b22b9dbabe644
yang <xingguoyang_mails@163.com>
2023-06-10 11:06:12 +0800

    copy-on-write

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,6 +9,11 @@
 #include "riscv.h"
 #include "defs.h"
 
+struct ref_stru{
+    struct spinlock lock;
+    int cnt[PHYSTOP/PGSIZE];  //引用计数
+}ref;
+
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
@@ -27,6 +32,7 @@ void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  initlock(&ref.lock,"ref");
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -36,7 +42,11 @@ freerange(void *pa_start, void *pa_end)
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+  {
+      // 在kfree中将会对cnt[]减1，这里要先设为1，否则就会减成负数
+      ref.cnt[(uint64)p / PGSIZE] = 1;
+      kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -51,15 +61,26 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+    // 只有当引用计数为0了才回收空间
+    // 否则只是将引用计数减1
+  acquire(&ref.lock);
+  if(--ref.cnt[(uint64)pa/PGSIZE]==0){
+      release(&ref.lock);
 
-  r = (struct run*)pa;
+      r=(struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+      // Fill with junk to catch dangling refs.
+      memset(pa, 1, PGSIZE);
+
+      r = (struct run*)pa;
+
+      acquire(&kmem.lock);
+      r->next = kmem.freelist;
+      kmem.freelist = r;
+      release(&kmem.lock);
+  }else{
+      release(&ref.lock);
+  }
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -72,8 +93,12 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
+  if(r){
+      kmem.freelist = r->next;
+      acquire(&ref.lock);
+      ref.cnt[(uint64)r/PGSIZE]=1;   //将引用计数初始化为1
+      release(&ref.lock);
+  }
   release(&kmem.lock);
 
   if(r)
