8b0ed53c0e5682c06fb57f64c723697828e7735c
yang <xingguoyang_mails@163.com>
2023-06-10 11:20:48 +0800

    copy-on-write

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -105,3 +105,90 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+/**
+ * @brief cowpage 判断一个页面是否为COW页面PTE_F(指向父进程的page)
+ * @param pagetable 指定查询的页表
+ * @param va 虚拟地址
+ * @return 0 是 -1 不是
+ */
+int cowpage(pagetable_t pagetable,uint64 va){
+    if(va>=MAXVA)
+        return -1;
+    pte_t * pte=walk(pagetable,va,0);
+    if(pte==0)
+        return -1;
+    if((*pte & PTE_V)==0)
+        return -1;
+    return (*pte & PTE_F?0:-1);
+}
+
+/**
+ * @brief cowalloc copy-on-write分配器
+ * @param pagetable 指定页表
+ * @param va 指定的虚拟地址,必须页面对齐
+ * @return 分配后va对应的物理地址，如果返回0则分配失败
+ */
+void* cowalloc(pagetable_t pagetable,uint64 va){
+    if(va%PGSIZE!=0)
+        return 0;
+    uint64 pa= walkaddr(pagetable,va);   //获取对应的物理地址
+    if(pa==0)
+        return 0;
+
+    pte_t* pte=walk(pagetable,va,0);    //获取对应的PTE
+
+    if(krefcnt((char*)pa)==1){
+        //只剩一个进程对此物理地址存在引用
+        //则直接修改对应的PTE即可
+        *pte|=PTE_W;
+        *pte&=~PTE_F;
+        return (void*)pa;
+    }else{
+        //多进程对物理内存存在引用
+        //需要分配新的页面，并拷贝旧页面的内容
+        char* mem=kalloc();
+        if(mem==0)
+            return 0;
+
+        //复制旧页面内容到新页
+        memmove(mem,(char*)pa,PGSIZE);
+
+        //清楚PTE_V，否则在mappages中会判定为remap
+        *pte &=~PTE_V;
+
+        // 为新页面添加映射
+        if(mappages(pagetable, va, PGSIZE, (uint64)mem, (PTE_FLAGS(*pte) | PTE_W) & ~PTE_F) != 0) {
+            kfree(mem);
+            *pte |= PTE_V;
+            return 0;
+        }
+
+        // 将原来的物理内存引用计数减1
+        kfree((char*)PGROUNDDOWN(pa));
+        return mem;
+    }
+}
+
+/**
+* @brief krefcnt 获取内存的引用计数
+* @param pa 指定的内存地址
+* @return 引用计数
+*/
+int krefcnt(void* pa) {
+    return ref.cnt[(uint64)pa / PGSIZE];
+}
+
+/**
+ * @brief kaddrefcnt 增加内存的引用计数
+ * @param pa 指定的内存地址
+ * @return 0:成功 -1:失败
+ */
+int kaddrefcnt(void* pa) {
+    if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+        return -1;
+    acquire(&ref.lock);
+    ++ref.cnt[(uint64)pa / PGSIZE];
+    release(&ref.lock);
+    return 0;
+}
